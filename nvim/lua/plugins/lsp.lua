0cd41c2408c602bbb6dbc93aa3c6660738a71aa9
Paulo Coelho <9609090+prscoelho@users.noreply.github.com>
2023-08-02 18:37:10 +0100

    Enable all rust features in lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -10,8 +10,23 @@ return {
         rust_analyzer = {
           settings = {
             ["rust-analyzer"] = {
+              cargo = {
+                allFeatures = true,
+                loadOutDirsFromCheck = true,
+                runBuildScripts = true,
+              },
               checkOnSave = {
+                allFeatures = true,
                 command = "clippy",
+                extraArgs = { "--no-deps" },
+              },
+              procMacro = {
+                enable = true,
+                ignored = {
+                  ["async-trait"] = { "async_trait" },
+                  ["napi-derive"] = { "napi" },
+                  ["async-recursion"] = { "async_recursion" },
+                },
               },
             },
           },

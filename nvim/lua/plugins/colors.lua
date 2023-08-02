a149266cc61e67faa6f00ec065b69a75bdf79d96
Paulo Coelho <9609090+prscoelho@users.noreply.github.com>
2023-08-02 18:37:17 +0100

    Some color tweaks

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -2,16 +2,16 @@ return {
   -- {
   --   "EdenEast/nightfox.nvim",
   -- },
-  {
-    "loctvl842/monokai-pro.nvim",
-  },
+  -- {
+  --   "loctvl842/monokai-pro.nvim",
+  -- },
   -- { "rktjmp/lush.nvim" },
   -- { dir = "~/Code/sitruuna.nvim/" },
   { "ellisonleao/gruvbox.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "monokai-pro-octagon",
+      colorscheme = "gruvbox",
     },
   },
 }
